e48239694e8c493a6c22a55ecb0c802f865f0bea
prprhyt <prprhyt@outlook.com>
2015-07-11 15:08:22 +0900

    Update pythonseleniumIEinstall.bat
    URLでBasic認証を通せるようにレジストリ変更の処理を加えた

--- pythonseleniumIEinstall.bat
+++ pythonseleniumIEinstall.bat
@@ -1,3 +1,4 @@
+reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_HTTP_USERNAME_PASSWORD_DISABLE" /v iexplore.exe /t REG_DWORD /d 0 /f
 powershell -Command "Set-ExecutionPolicy RemoteSigned"
 set dir=%~dp0
 powershell -Command "Start-Process powershell.exe -ArgumentList %dir%pythoninstall.ps1 -Verb runas"
