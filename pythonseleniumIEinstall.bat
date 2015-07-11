d14374b22ed13c8f51a96fe366c38b77967042a3
prprhyt <prprhyt@outlook.com>
2015-07-11 14:50:53 +0900

    Update pythonseleniumIEinstall.bat
    配置ディレクトリに依存しないように変更

--- pythonseleniumIEinstall.bat
+++ pythonseleniumIEinstall.bat
@@ -1,6 +1,6 @@
 powershell -Command "Set-ExecutionPolicy RemoteSigned"
-
-powershell -Command "Start-Process powershell.exe -ArgumentList C:\pythonseleniumsetup\pythoninstall.ps1 -Verb runas"
+set dir=%~dp0
+powershell -Command "Start-Process powershell.exe -ArgumentList %dir%pythoninstall.ps1 -Verb runas"
 
 pip install --upgrade pip
 pip install selenium
