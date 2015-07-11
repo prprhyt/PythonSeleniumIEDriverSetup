2c351b6304238cc3a031642dd2cbd34595415914
prprhyt <prprhyt@outlook.com>
2015-07-11 14:20:04 +0900

    Update pythoninstall.ps1
    フォルダ名に依存せず実行できるように変更

--- pythoninstall.ps1
+++ pythoninstall.ps1
@@ -1,7 +1,11 @@
 New-Item C:\Python27 -itemType Directory
-(new-object System.Net.WebClient).DownloadFile("https://www.python.org/ftp/python/2.7.10/python-2.7.10.msi", "C:\\pythonseleniumsetup\\python27installer.msi");
-msiexec /i "C:\\pythonseleniumsetup\\python27installer.msi" /l*v "C:\\pythonseleniumsetup\\python27install.log" /qn;
+$mypath =Split-Path ( & { $myInvocation.ScriptName } ) -parent;
+$usepath1=$mypath+"\\python27installer.msi";
+(new-object System.Net.WebClient).DownloadFile("https://www.python.org/ftp/python/2.7.10/python-2.7.10.msi", $usepath1);
+$usepath1= $mypath + "\\python27installer.msi";
+$usepath2 = $mypath +"\\python27install.log";msiexec /i $usepath1 /l*v $usepath2 /qn;
 $path = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
 $path += ';' + 'C:\Python27;C:\Python27\Scripts';
 [Environment]::SetEnvironmentVariable('PATH', $path, 'Machine');
-Copy-Item C:\pythonseleniumsetup\IEDriverServer.exe C:\Python27 -Force
+$usepath1=$mypath+"\IEDriverServer.exe";
+Copy-Item $usepath1 C:\Python27 -Force
