80afd8e0d66fb2ce8705dd6932c7ac359f4f69a1
prprhyt <prprhyt@outlook.com>
2015-07-11 17:03:09 +0900

    Update PythonIEDriverInstall.ps1
    再起動処理を追加した

--- PythonIEDriverInstall.ps1
+++ PythonIEDriverInstall.ps1
@@ -1,11 +1,14 @@
-New-Item C:\Python27 -itemType Directory
+New-Item C:\Python27 -itemType Directory -Force
 $mypath =Split-Path ( & { $myInvocation.ScriptName } ) -parent;
 $usepath1=$mypath+"\\python27installer.msi";
 (new-object System.Net.WebClient).DownloadFile("https://www.python.org/ftp/python/2.7.10/python-2.7.10.msi", $usepath1);
 $usepath1= $mypath + "\\python27installer.msi";
-$usepath2 = $mypath +"\\python27install.log";msiexec /i $usepath1 /l*v $usepath2 /qn;
+$usepath2 = $mypath +"\\python27install.log";
+Start-Process -FilePath msiexec -ArgumentList /i, $usepath1, /quiet -Wait
 $path = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
 $path += ';' + 'C:\Python27;C:\Python27\Scripts';
 [Environment]::SetEnvironmentVariable('PATH', $path, 'Machine');
 $usepath1=$mypath+"\IEDriverServer.exe";
 Copy-Item $usepath1 C:\Python27 -Force
+$usepath1 = "start "+$mypath+"\\updatecmd.bat"
+cmd /c "shutdown -r -t 10"
