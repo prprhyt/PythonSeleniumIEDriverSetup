591936a891bc77a0b41e616c7bc0a9b74703f555
prprhyt <prprhyt@outlook.com>
2015-07-11 17:02:29 +0900

    Update startcmd.bat
    アップデート処理をupdatecmd.batに移した

--- startcmd.bat
+++ startcmd.bat
@@ -1,7 +1,4 @@
 reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_HTTP_USERNAME_PASSWORD_DISABLE" /v iexplore.exe /t REG_DWORD /d 0 /f
 powershell -Command "Set-ExecutionPolicy RemoteSigned"
 set dir=%~dp0
-powershell -Command "Start-Process powershell.exe -ArgumentList %dir%pythoninstall.ps1 -Verb runas"
-
-pip install --upgrade pip
-pip install selenium
+powershell -Command "Start-Process powershell.exe -ArgumentList %dir%PythonIEDriverInstall.ps1 -Verb runas"
